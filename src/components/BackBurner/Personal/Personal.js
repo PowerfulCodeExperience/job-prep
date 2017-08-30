82ca27d8ee17680dd65126dfec1fc80c2e4c3612
Carter Hatch <carterhatch@live.com>
2017-08-30 17:15:51 -0600

    minor tweak

--- src/components/BackBurner/Personal/Personal.js
+++ src/components/BackBurner/Personal/Personal.js
@@ -1,13 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import './Personal.css';
 
 class Personal extends Component {
-  render() {
-    return (
+  render(){
+
+    const userImage = () => {
+      if(this.props.user.picture) {
+        return <img src={this.props.user.picture} alt="" />
+      }
+      else {
+        return <img src={require("./user-default.png")} alt="" />
+      }
+    }
+
+    return(
       <div className="Personal">
-        Personal
+
+        <div className="Status">
+          4:20 PM, August 30, 2017
+          <span>Status: Unemployed :(</span>
+        </div>
+
+        <header className="TitleBar">
+          {userImage()}
+          Good Morning {this.props.user.firstname || 'Bucko'}!
+        </header>
+
       </div>
     )
   }
@@ -15,7 +35,7 @@ class Personal extends Component {
 
 function mapStateToProps(state) {
   return {
-
+    user: state.user
   }
 }
 
